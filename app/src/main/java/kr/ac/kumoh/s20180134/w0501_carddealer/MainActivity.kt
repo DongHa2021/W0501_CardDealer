37267378fb79e9d776982f27e77151bb89bdd830
DongHa <kdh2900@kakao.com>
2022-10-04 14:41:24 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20180134/w0501_carddealer/MainActivity.kt
@@ -0,0 +1,16 @@
+package kr.ac.kumoh.s20180134.w0501_carddealer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20180134.w0501_carddealer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate((layoutInflater))
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+}
