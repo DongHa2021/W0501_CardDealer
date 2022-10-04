fd0851d0f04243457ee5dcb09a81dfafa051d82a
DongHa <kdh2900@kakao.com>
2022-10-04 15:49:59 +0900

    10.04

--- app/src/main/java/kr/ac/kumoh/s20180134/w0501_carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180134/w0501_carddealer/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20180134.w0501_carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20180134.w0501_carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,27 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
+        getCardName(29)
+
         binding.card1.setImageResource(R.drawable.c_10_of_spades)
     }
+
+    private fun getCardName(c: Int) : String {
+        Log.i("getCardName : ",(c/13).toString())
+        val shape = when (c/13){
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else ->"error"
+        }
+        Log.i("getCardName : ",shape)
+        val number = when(c%13){
+            in 1..9 ->(c%13+1).toString()
+            else ->"error"
+        }
+        Log.i("getCardName : ",number)
+        return "c_ace_of_spades"
+    }
+
 }
